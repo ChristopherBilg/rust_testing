b7b88bd7327f98ff15acf4eca36764dc074d8f4a
Christopher Bilger <christopherbilg@gmail.com>
2019-12-03 16:31:25 -0500

    Fixed array initialization

--- main.rs
+++ main.rs
@@ -1,12 +1,15 @@
 fn main() {
     // Arrays in Rust must be initialized at compile time
-    let x: [i32; 5] = [0, 1, 2, 3, 4];
+    // Array size = 10, and initialized to 5
+    const SIZE: usize = 10;
+    let initial_value: i32 = 5;
+    let x: [i32; SIZE] = [initial_value; SIZE];
 
-    print_array(x);
+    print_array(&x);
     println!("");
 }
 
-fn print_array(arr: [i32; 5]) {
+fn print_array(arr: &[i32]) {
     for item in arr.iter() {
         print!("{} ", item);
     }
