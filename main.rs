5adffb6e84cca4cf3e29c25b698c750af0669123
Christopher Bilger <christopherbilg@gmail.com>
2019-12-03 16:19:44 -0500

    Started learning about arrays (always fixed size)

--- main.rs
+++ main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Test!");
+    // Arrays in Rust must be initialized at compile time
+    let x: [i32; 5] = [0, 1, 2, 3, 4];
+
+    print_array(x);
+    println!("");
+}
+
+fn print_array(arr: [i32; 5]) {
+    for item in arr.iter() {
+        print!("{} ", item);
+    }
 }
