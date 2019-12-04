78cdafff5683b8f95dd70dad9b4f3118de8db12d
Christopher Bilger <christopherbilg@gmail.com>
2019-12-04 09:05:36 -0500

    Slight chnages to array size

--- main.rs
+++ main.rs
@@ -2,10 +2,10 @@
 
 fn main() {
     // Arrays in Rust must be initialized at compile time
-    // Array size = 10, and initialized to 5
-    const SIZE: usize = 10;
-    let initial_value: i32 = 5;
-    let x: [i32; SIZE] = [initial_value; SIZE];
+    // Array size = 20, and initialized to 5
+    const SIZE: usize = 20;
+    const INIT_VALUE: i32 = 5;
+    let x: [i32; SIZE] = [INIT_VALUE; SIZE];
 
     print_array(&x);
     println!("");
